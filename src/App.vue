7ad9d7f9ce9ffccb7f31ab515ff91749214b6b14
Nick <zhongcheng509@gmail.com>
2017-12-21 10:09:52 +0800

    vue-music还原

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png" alt="">
-    <!--这里是一级路由-->
-    <router-view></router-view>
-    <router-view class="left" name="title"></router-view>
-    <router-view class="right" name="img"></router-view>
+    hello world
   </div>
 </template>
 
@@ -16,10 +12,6 @@
   @import "common/stylus/variable.styl";
   #app
     color: $color-theme
-    text-align: center
-  .left, .right
-    width:49%;
-    border:1px solid gray;
-    float:left;
+
 
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,40 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import GoodsList from './../views/GoodsList'
-import Title from '@/views/Title'
-import Image from '@/views/Image'
-import Cart from '@/views/Cart'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'hash',
-  routes: [
-    {
-      path: '/goods',
-      name: 'GoodsList',
-      // component: GoodsList,
-      components: {
-        default: GoodsList,
-        title: Title,
-        img: Image
-      },
-      children: [
-        {
-          path: 'title', // 路径，不要加'/'
-          name: 'title', // 名字
-          component: Title // 组件名
-        },
-        {
-          path: 'img',
-          name: 'img',
-          component: Image
-        }
-      ]
-    }, {
-      path: '/cart/:cartId',
-      name: 'cart',
-      component: Cart
-    }
-  ]
+  routes: []
 })
