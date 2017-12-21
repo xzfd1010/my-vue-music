904d8171867469fb2ba80afde63e9a82ed83ac77
Nick <zhongcheng509@gmail.com>
2017-12-21 19:25:12 +0800

    配置路由

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -36,7 +36,9 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
-      'common':resolve('src/common')
+      'common':resolve('src/common'),
+      'components':resolve('src/components'),
+      'views':resolve('src/views')
     }
   },
   module: {
--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <div id="app">
-    hello world
+    <m-header></m-header>
+    <tab></tab>
+    <router-view></router-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import MHeader from 'components/m-header/m-header'
+  import Tab from 'components/tab/tab'
 
+  export default {
+    components: {
+      MHeader,
+      Tab
+    }
+  }
 </script>
 
 <style lang="stylus" type="text/stylus">
