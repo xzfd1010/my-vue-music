6d394a2f4b677f05c2078952273604fb45ccf338
Nick <zhongcheng509@gmail.com>
2017-12-22 17:36:54 +0800

    获取数据

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -38,7 +38,8 @@ module.exports = {
       '@': resolve('src'),
       'common':resolve('src/common'),
       'components':resolve('src/components'),
-      'views':resolve('src/views')
+      'views':resolve('src/views'),
+      'api':resolve('src/api')
     }
   },
   module: {
