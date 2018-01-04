d2b40a107e9c7955b285ca72f3e6d7be31d3ced4
Nick <zhongcheng509@gmail.com>
2018-01-04 16:36:32 +0800

    播放error事件处理及按钮切换效果

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -31,13 +31,13 @@
             <div class="icon i-left">
               <i class="icon-sequence"></i>
             </div>
-            <div class="icon i-left">
+            <div class="icon i-left" :class="disableCls">
               <i class="icon-prev" @click="prev"></i>
             </div>
-            <div class="icon i-center">
+            <div class="icon i-center" :class="disableCls">
               <i :class="playIcon" @click="togglePlaying"></i>
             </div>
-            <div class="icon i-right">
+            <div class="icon i-right" :class="disableCls">
               <i class="icon-next" @click="next"></i>
             </div>
             <div class="icon i-right">
@@ -90,6 +90,9 @@
       },
       miniIcon() {
         return this.playing ? 'icon-pause-mini' : 'icon-play-mini'
+      },
+      disableCls() {
+        return this.songReady ? '' : 'disable'
       },
       ...mapGetters([
         'fullScreen',
@@ -187,7 +190,8 @@
         this.songReady = true
       },
       error() {
-
+        // 报错时，如果不修改状态，播放等功能就无法触发
+        this.songReady = true
       },
       _getPosAndScale() {
         // 计算距离的方法
