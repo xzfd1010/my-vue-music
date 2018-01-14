02a7db20273d94fbda2d21a1faca5f5c65e83207
Nick <zhongcheng509@gmail.com>
2018-01-14 11:22:56 +0800

    获取热门搜索数据

--- src/views/search/search.vue
+++ src/views/search/search.vue
@@ -1,16 +1,50 @@
 <template>
   <div class="search">
     <div class="search-box-wrapper">
-      <search-box></search-box>
+      <search-box ref="searchBox"></search-box>
+    </div>
+    <div class="shortcut-wrapper">
+      <div class="shortcut">
+        <div class="hot-key">
+          <h1 class="title">热门搜索</h1>
+          <ul>
+            <li @click="addQuery(item.k)" class="item" v-for="item in hotKey">
+              <span>{{item.k}}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
   import SearchBox from 'base/search-box/search-box'
+  import {getHotKey} from '../../api/search'
+  import {ERR_OK} from '../../api/config'
 
   export default {
     name: '',
+    data() {
+      return {
+        hotKey: []
+      }
+    },
+    created() {
+      this._getHotKey()
+    },
+    methods: {
+      addQuery(query) {
+        this.$refs.searchBox.setQuery(query)
+      },
+      _getHotKey() {
+        getHotKey().then(res => {
+          if (res.code === ERR_OK) {
+            this.hotKey = res.data.hotkey.slice(0, 10)
+          }
+        })
+      }
+    },
     components: {
       SearchBox
     }
